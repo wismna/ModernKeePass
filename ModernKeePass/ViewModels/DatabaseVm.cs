8ada0507c2df2739e90988336680e420f27e5a83
bg45 <bonneville.geoffroy@rcgt.com>
2017-09-13 16:48:05 -0400

    Some changes PAT info

--- ModernKeePass/ViewModels/DatabaseVm.cs
+++ ModernKeePass/ViewModels/DatabaseVm.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Collections.ObjectModel;
-using System.Linq;
-using Windows.Storage;
+﻿using Windows.Storage;
 
 using ModernKeePassLib;
 using ModernKeePassLib.Keys;
