b2c8f2e0f4988ac21047ee9305683ad2eadec98b
Geoffroy Bonneville <geogeob@hotmail.com>
2017-11-14 16:22:37 +0000

    Updated ImportCertificate.ps1

--- Scripts/ImportCertificate.ps1
+++ Scripts/ImportCertificate.ps1
@@ -4,4 +4,4 @@
 param(
     [Parameter(Mandatory=$true)][string]$CertPath
 )
-Write-Host "Registering certificate: $CertPath"
+Write-Host "Registering certificate: $CertPath" -CertStoreLocation cert:\CurrentUser\Root
