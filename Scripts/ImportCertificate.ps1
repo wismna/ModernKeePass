ea481187d5e393a79413e41796b393ebd1117dfe
BONNEVILLE Geoffroy <gbonneville@mega.com>
2017-11-14 17:03:24 +0100

    Add build scripts

---
+++ Scripts/ImportCertificate.ps1
@@ -0,0 +1,8 @@
+﻿# Enable -Verbose option
+[CmdletBinding()]
+
+param(
+    [string]$CertPath = "$Env:BUILD_ARTIFACTSTAGINGDIRECTORY",
+    [Parameter(Mandatory=$true)][string]$FileName
+)
+Import-Certificate -Filepath "$CertPath\$FileName"
